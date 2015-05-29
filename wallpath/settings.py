5ed1771361714b87140c881095a857b70d11f7ba
kyaw Mong <ekyawthan@gmail.com>
2015-05-29 18:13:35 -0400

    change database

--- wallpath/settings.py
+++ wallpath/settings.py
@@ -62,8 +62,10 @@ WSGI_APPLICATION = 'wallpath.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wallpath',
+        'USER': 'wallpath',
+        'PASSWORD': 'password123',
     }
 }
 
